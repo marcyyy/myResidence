cfca67544bee807357bf0ff338573f24541d4f9d
Zyrah <avila.zyrah@gmail.com>
2021-11-29 04:44:33 +0800

    update

--- TPS/settings.py
+++ TPS/settings.py
@@ -279,7 +279,6 @@ JAZZMIN_UI_TWEAKS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
