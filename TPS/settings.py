08eb8f22eba7c3ab9728baf2c062fb6d34a40a75
Zyrah <avila.zyrah@gmail.com>
2021-11-29 04:14:05 +0800

    update

--- TPS/settings.py
+++ TPS/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tpsdb',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'USER': 'root',
         'PASSWORD': '',
         'PORT': '3306',
