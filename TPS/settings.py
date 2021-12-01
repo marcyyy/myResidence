a9bc432a5558c89fca08b934bbb998598a85b5bc
Zyrah <avila.zyrah@gmail.com>
2021-12-02 02:09:54 +0800

    Update

--- TPS/settings.py
+++ TPS/settings.py
@@ -133,6 +133,16 @@ USE_TZ = False
 
 LANGUAGES = DJANGO_LANGUAGES
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # MEDIA_ROOT = os.path.join(BASE_DIR, "/static/uploaded/profile/")
 
 # Default primary key field type
@@ -278,15 +288,5 @@ JAZZMIN_UI_TWEAKS = {
     },
     "actions_sticky_top": False
 }
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
